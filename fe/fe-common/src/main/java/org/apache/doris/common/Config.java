5b5ed6bd7b63b1de66cf53b4f7a725d6c3cbe1b9
cambyzhu <cambyzhu@tencent.com>
2023-12-15 11:15:36 +0800

    [Tencent] adjust stream load default timeout from 3days to 12hours

--- fe/fe-common/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/Config.java
@@ -584,7 +584,7 @@ public class Config extends ConfigBase {
      * Default stream load and streaming mini load timeout
      */
     @ConfField(mutable = true, masterOnly = true)
-    public static int stream_load_default_timeout_second = 86400 * 3; // 3days
+    public static int stream_load_default_timeout_second = 43200; // 12 hours
 
     /**
      * Default stream load pre-commit status timeout
