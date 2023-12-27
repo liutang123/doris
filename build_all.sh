4873d834a40d2126494ab5dceead685e5bdd00af
derenli <derenli@tencent.com>
2023-12-27 16:57:01 +0800

    [Tencent] [compile] Fix a building error of apache-orc and clucene

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 set -x
 
-HOME_DIR=`pwd`
+curdir=$(dirname "$0")
+curdir=$(
+  cd "$curdir"
+  pwd
+)
+HOME_DIR=$curdir
+
+COS_URL="https://derenli-1301087413.cos.ap-guangzhou.myqcloud.com/doris_test"
+
+rm -fr "${HOME_DIR}/be/src/apache-orc"
+rm -fr "${HOME_DIR}/be/src/clucene"
+
+if [[ ! -f "${HOME_DIR}/apache-orc-branch20.tgz" ]]; then
+  wget -q "${COS_URL}/apache-orc-branch20.tgz" -P "${HOME_DIR}"
+fi
+
+if [[ ! -f "${HOME_DIR}/clucene-branch20.tgz" ]]; then
+  wget -q "${COS_URL}/clucene-branch20.tgz" -P "${HOME_DIR}"
+fi
+
+tar zxf "${HOME_DIR}/apache-orc-branch20.tgz" -C "${HOME_DIR}/be/src"
+tar zxf "${HOME_DIR}/clucene-branch20.tgz" -C "${HOME_DIR}/be/src"
 
 # clean and build all
 ./build.sh --clean --fe --be --broker --audit --spark-dpp --hive-udf -j90
