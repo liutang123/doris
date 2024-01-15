addad58feae2465f93c60ae63485faf1f2586716
derenli <derenli@tencent.com>
2024-01-15 17:08:31 +0800

    [Tencent] [scripts] download maven repo to speed up for the first time to build

--- build_all.sh
+++ build_all.sh
@@ -8,29 +8,40 @@ curdir=$(
 )
 WORK_DIR=$curdir
 
-COS_URL="https://derenli-1301087413.cos.ap-guangzhou.myqcloud.com/doris_release_package/thridparty"
+COS_URL="https://derenli-1301087413.cos.ap-guangzhou.myqcloud.com/doris_release_package"
+
+THIRDPARTY_COS_URL="${COS_URL}/thirdparty"
+MAVEN_REPO_COS_URL="${COS_URL}/maven_repo"
 THIRDPARTY_DIR="${WORK_DIR}/thirdparty"
 
 # update apache-orc
 rm -fr "${WORK_DIR}/be/src/apache-orc"
 if [[ ! -f "${THIRDPARTY_DIR}/apache-orc-branch20.tgz" ]]; then
-  wget -q "${COS_URL}/apache-orc-branch20.tgz" -P "${THIRDPARTY_DIR}"
+  wget -q "${THIRDPARTY_COS_URL}/apache-orc-branch20.tgz" -P "${THIRDPARTY_DIR}"
 fi
 tar zxf "${THIRDPARTY_DIR}/apache-orc-branch20.tgz" -C "${WORK_DIR}/be/src"
 
 # update CLucece
 rm -fr "${WORK_DIR}/be/src/clucene"
 if [[ ! -f "${THIRDPARTY_DIR}/clucene-branch20.tgz" ]]; then
-  wget -q "${COS_URL}/clucene-branch20.tgz" -P "${THIRDPARTY_DIR}"
+  wget -q "${THIRDPARTY_COS_URL}/clucene-branch20.tgz" -P "${THIRDPARTY_DIR}"
 fi
 tar zxf "${THIRDPARTY_DIR}/clucene-branch20.tgz" -C "${WORK_DIR}/be/src"
 
 # update other thirdparty libs
 rm -fr "/var/local/thirdparty/installed"
 if [[ ! -f "${THIRDPARTY_DIR}/doris-2.x-thirdparty-libs.tar.gz" ]]; then
-  wget -q "${COS_URL}/doris-2.x-thirdparty-libs.tar.gz" -P "${THIRDPARTY_DIR}"
+  wget -q "${THIRDPARTY_COS_URL}/doris-2.x-thirdparty-libs.tar.gz" -P "${THIRDPARTY_DIR}"
 fi
 tar zxf "${THIRDPARTY_DIR}/doris-2.x-thirdparty-libs.tar.gz" -C "/var/local/thirdparty"
+
+# download maven repo to speed up for the first time to build
+if [[ ! -d "${WORK_DIR}/.m2" ]]; then
+  if [[ ! -f "${MAVEN_REPO_COS_URL}/maven_repo_m2_cdwdoris-2.0.tar.gz" ]]; then
+    wget -q "${MAVEN_REPO_COS_URL}/maven_repo_m2_cdwdoris-2.0.tar.gz" -P "${WORK_DIR}"
+  fi
+  tar zxf "${WORK_DIR}/maven_repo_m2_cdwdoris-2.0.tar.gz" -C "${WORK_DIR}"
+fi
 
 # clean and build all
 sh ${WORK_DIR}/build.sh --clean --fe --be --broker --audit --spark-dpp --hive-udf -j90
