2165298e40feb7d86667050fb8daa8b8edf4a8d2
derenli <derenli@tencent.com>
2024-03-20 21:26:56 +0800

    [Tencent] Revert "[fix](planner)need call materializeSrcExpr for materialized slots in join node (#25204) (#31016)"
    This reverts commit 07d30d46ff13ff7622fea4074ff14d37763c4081.

--- fe/fe-core/src/main/java/org/apache/doris/planner/JoinNodeBase.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/JoinNodeBase.java
@@ -168,6 +168,9 @@ public abstract class JoinNodeBase extends PlanNode {
                 boolean needSetToNullable =
                         getChild(0) instanceof JoinNodeBase && analyzer.isOuterJoined(leftTupleDesc.getId());
                 for (SlotDescriptor leftSlotDesc : leftTupleDesc.getSlots()) {
+                    if (!isMaterializedByChild(leftSlotDesc, getChild(0).getOutputSmap())) {
+                        continue;
+                    }
                     SlotDescriptor outputSlotDesc =
                             analyzer.getDescTbl().copySlotDescriptor(vOutputTupleDesc, leftSlotDesc);
                     if (leftNullable) {
@@ -188,6 +191,9 @@ public abstract class JoinNodeBase extends PlanNode {
                 boolean needSetToNullable =
                         getChild(1) instanceof JoinNodeBase && analyzer.isOuterJoined(rightTupleDesc.getId());
                 for (SlotDescriptor rightSlotDesc : rightTupleDesc.getSlots()) {
+                    if (!isMaterializedByChild(rightSlotDesc, getChild(1).getOutputSmap())) {
+                        continue;
+                    }
                     SlotDescriptor outputSlotDesc =
                             analyzer.getDescTbl().copySlotDescriptor(vOutputTupleDesc, rightSlotDesc);
                     if (rightNullable) {
@@ -220,7 +226,6 @@ public abstract class JoinNodeBase extends PlanNode {
                 rSlotRef.getDesc().setIsMaterialized(lSlotRef.getDesc().isMaterialized());
             } else {
                 rSlotRef.getDesc().setIsMaterialized(true);
-                rSlotRef.materializeSrcExpr();
             }
         }
 
