00dc842c4a07f7fc01747c1cf1879e014442fdd7
garenshi <garenshi@tencent.com>
2024-06-18 10:28:52 +0800

    [Tencent] remove cool down time limit for storage policy

--- fe/fe-common/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/Config.java
@@ -3396,4 +3396,7 @@ public class Config extends ConfigBase {
     @ConfField(mutable = true, description = {"Prometheus 输出表维度指标的个数限制",
             "Prometheus output table dimension metric count limit"})
     public static int prom_output_table_metrics_limit = 10000;
+
+    @ConfField(mutable = true, masterOnly = true, description = {})
+    public static boolean ignore_cool_down_time_check = true;
 }
