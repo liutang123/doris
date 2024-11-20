aa8c8855ce968bbdfd7698ba8b62a755cc1cb275
derenli <derenli@tencent.com>
2024-11-20 11:54:10 +0800

    [Tencent] Suppress unexpected warnings only set enable_cpu_hard_limit is true

--- fe/fe-core/src/main/java/org/apache/doris/resource/workloadgroup/WorkloadGroup.java
+++ fe/fe-core/src/main/java/org/apache/doris/resource/workloadgroup/WorkloadGroup.java
@@ -133,7 +133,7 @@ public class WorkloadGroup implements Writable, GsonPostProcessable {
     private int maxQueueSize = 0;
     private int queueTimeout = 0;
 
-    private int cpuHardLimit = 0;
+    private int cpuHardLimit = -1;
 
     WorkloadGroup(long id, String name, Map<String, String> properties) {
         this(id, name, properties, 0);
@@ -591,6 +591,14 @@ public class WorkloadGroup implements Writable, GsonPostProcessable {
         String memOvercommitStr = properties.get(ENABLE_MEMORY_OVERCOMMIT);
         if (memOvercommitStr != null) {
             tWorkloadGroupInfo.setEnableMemoryOvercommit(Boolean.valueOf(memOvercommitStr));
+        }
+        // enable_cpu_hard_limit = true, using cpu hard limit
+        // enable_cpu_hard_limit = false, using cpu soft limit
+        tWorkloadGroupInfo.setEnableCpuHardLimit(Config.enable_cpu_hard_limit);
+
+        if (Config.enable_cpu_hard_limit && !(cpuHardLimit >= 1 && cpuHardLimit <= 100) && -1 != cpuHardLimit) {
+            LOG.warn("enable_cpu_hard_limit=true but cpuHardLimit value not illegal,"
+                    + "id=" + id + ",name=" + name);
         }
 
         String scanThreadNumStr = properties.get(SCAN_THREAD_NUM);
