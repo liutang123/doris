242c07c1ddb03c34260bfa036377cea902e758aa
derenli <derenli@tencent.com>
2024-11-22 14:54:42 +0800

    [Tencent] Check and show debug commit which need to revert

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -2,6 +2,38 @@
 
 WORK_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
+# check if or not has debug commit need to revert
+ignored_commits=(
+  "78d99c6fdfe41e3fc0979c0e1feead8ab6a3e217"
+  "9579634eac68e6f65099a1f4f78687f36eff3a23"
+  "4de21d604afe0779b34de8586e3fbfd618551054"
+)
+
+commits_to_revert=()
+commits=$(git log --grep="^\[Debug" --pretty=format:"%H")
+if [ ! -z "$commits" ]; then
+  for commit in $commits; do
+    if [[ " ${ignored_commits[@]} " =~ " ${commit} " ]]; then
+      #echo "Commit $commit is in the ignore list. Skipping..."
+      continue
+    fi
+    if git log --grep="This reverts commit $commit" --format=%B | grep -q "This reverts commit $commit"; then
+      #echo "Commit $commit has already been reverted. Skipping..."
+      continue
+    fi
+    #echo "Need to revert commit $commit..."
+    commits_to_revert+=("$commit")
+  done
+fi
+
+if [ ${#commits_to_revert[@]} -gt 0 ]; then
+  echo "The following commits need to be reverted:"
+  for commit in "${commits_to_revert[@]}"; do
+    echo "$commit"
+  done
+  exit 1
+fi
+
 # start docker and pull image
 systemctl start docker
 docker pull apache/doris:build-env-ldb-toolchain-latest
