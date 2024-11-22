a240024c43acd033d13fe4cc3182cdeb22ce3c1c
derenli <derenli@tencent.com>
2024-11-22 14:20:11 +0800

    [Tencent] Revert "[Debug](float) Add clang debug tune float accuracy (#25041)"
    This reverts commit a9d12f7b8208479ccaa448f2472eea01e61f2115.

--- be/src/vec/aggregate_functions/aggregate_function_avg.h
+++ be/src/vec/aggregate_functions/aggregate_function_avg.h
@@ -142,9 +142,6 @@ public:
 
     void add(AggregateDataPtr __restrict place, const IColumn** columns, ssize_t row_num,
              Arena*) const override {
-#ifdef __clang__
-#pragma clang fp reassociate(on)
-#endif
         const auto& column = assert_cast<const ColVecType&>(*columns[0]);
         if constexpr (IsDecimalNumber<T>) {
             this->data(place).sum += column.get_data()[row_num].value;
