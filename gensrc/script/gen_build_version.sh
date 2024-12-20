6c34756d49159d8a7cbffe9c3158e6fd3b01bd6d
derenli <derenli@tencent.com>
2024-12-20 18:58:16 +0800

    [Tencent] Change to 2.1.8

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -30,7 +30,7 @@ set -eo pipefail
 build_version_prefix="tencent-cdw-doris"
 build_version_major="2"
 build_version_minor="1"
-build_version_patch="7"
+build_version_patch="8"
 
 # The patch nums for tencent
 user=doris
