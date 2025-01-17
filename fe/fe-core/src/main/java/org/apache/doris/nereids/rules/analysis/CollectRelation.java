18c27137342cfe8ab29b7e36491061abb46fe250
shee <13843187+qzsee@users.noreply.github.com>
2025-01-17 11:02:22 +0800

    [Tencent] [chore] change info to debug for collecting mtmv candidates (#47072)

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/CollectRelation.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/CollectRelation.java
@@ -208,7 +208,9 @@ public class CollectRelation implements AnalysisRuleFactory {
         if (shouldCollect) {
             Set<MTMV> mtmvSet = Env.getCurrentEnv().getMtmvService().getRelationManager()
                     .getAllMTMVs(Lists.newArrayList(new BaseTableInfo(table)));
-            LOG.info("table {} related mv set is {}", new BaseTableInfo(table), mtmvSet);
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("table {} related mv set is {}", new BaseTableInfo(table), mtmvSet);
+            }
             for (MTMV mtmv : mtmvSet) {
                 cascadesContext.getStatementContext().getMtmvRelatedTables().put(mtmv.getFullQualifiers(), mtmv);
                 mtmv.readMvLock();
