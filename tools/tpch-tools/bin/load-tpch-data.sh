84957d6b3642e99f2e5b696ef016f0edf90ad13a
derenli <derenli@tencent.com>
2025-03-10 19:42:47 +0800

    [Tencent](tools) Fix loading failure in tpch/ssb tools by adding 'Expect: 100-continue'

--- tools/tpch-tools/bin/load-tpch-data.sh
+++ tools/tpch-tools/bin/load-tpch-data.sh
@@ -126,11 +126,13 @@ function load_region() {
     if [[ -z ${TXN_ID} ]]; then
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "columns: r_regionkey, r_name, r_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/region/_stream_load
     else
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "label:${TXN_ID}_region" \
             -H "columns: r_regionkey, r_name, r_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/region/_stream_load
     fi
 }
@@ -139,11 +141,13 @@ function load_nation() {
     if [[ -z ${TXN_ID} ]]; then
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "columns: n_nationkey, n_name, n_regionkey, n_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/nation/_stream_load
     else
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "label:${TXN_ID}_nation" \
             -H "columns: n_nationkey, n_name, n_regionkey, n_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/nation/_stream_load
     fi
 }
@@ -152,11 +156,13 @@ function load_supplier() {
     if [[ -z ${TXN_ID} ]]; then
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "columns: s_suppkey, s_name, s_address, s_nationkey, s_phone, s_acctbal, s_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/supplier/_stream_load
     else
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "label:${TXN_ID}_supplier" \
             -H "columns: s_suppkey, s_name, s_address, s_nationkey, s_phone, s_acctbal, s_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/supplier/_stream_load
     fi
 }
@@ -165,11 +171,13 @@ function load_customer() {
     if [[ -z ${TXN_ID} ]]; then
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "columns: c_custkey, c_name, c_address, c_nationkey, c_phone, c_acctbal, c_mktsegment, c_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/customer/_stream_load
     else
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "label:${TXN_ID}_customer" \
             -H "columns: c_custkey, c_name, c_address, c_nationkey, c_phone, c_acctbal, c_mktsegment, c_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/customer/_stream_load
     fi
 }
@@ -178,11 +186,13 @@ function load_part() {
     if [[ -z ${TXN_ID} ]]; then
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "columns: p_partkey, p_name, p_mfgr, p_brand, p_type, p_size, p_container, p_retailprice, p_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/part/_stream_load
     else
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "label:${TXN_ID}_part" \
             -H "columns: p_partkey, p_name, p_mfgr, p_brand, p_type, p_size, p_container, p_retailprice, p_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/part/_stream_load
     fi
 }
@@ -193,11 +203,13 @@ function load_partsupp() {
     if [[ -z ${TXN_ID} ]]; then
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "columns: ps_partkey, ps_suppkey, ps_availqty, ps_supplycost, ps_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/partsupp/_stream_load
     else
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "label:${TXN_ID}_partsupp_${FILE_ID}" \
             -H "columns: ps_partkey, ps_suppkey, ps_availqty, ps_supplycost, ps_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/partsupp/_stream_load
     fi
 }
@@ -208,11 +220,13 @@ function load_orders() {
     if [[ -z ${TXN_ID} ]]; then
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "columns: o_orderkey, o_custkey, o_orderstatus, o_totalprice, o_orderdate, o_orderpriority, o_clerk, o_shippriority, o_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/orders/_stream_load
     else
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "label:${TXN_ID}_orders_${FILE_ID}" \
             -H "columns: o_orderkey, o_custkey, o_orderstatus, o_totalprice, o_orderdate, o_orderpriority, o_clerk, o_shippriority, o_comment, temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/orders/_stream_load
     fi
 }
@@ -223,11 +237,13 @@ function load_lineitem() {
     if [[ -z ${TXN_ID} ]]; then
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "columns: l_orderkey, l_partkey, l_suppkey, l_linenumber, l_quantity, l_extendedprice, l_discount, l_tax, l_returnflag,l_linestatus, l_shipdate,l_commitdate,l_receiptdate,l_shipinstruct,l_shipmode,l_comment,temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/lineitem/_stream_load
     else
         curl -s --location-trusted -u "${USER}":"${PASSWORD}" -H "column_separator:|" \
             -H "label:${TXN_ID}_lineitem_${FILE_ID}" \
             -H "columns: l_orderkey, l_partkey, l_suppkey, l_linenumber, l_quantity, l_extendedprice, l_discount, l_tax, l_returnflag,l_linestatus, l_shipdate,l_commitdate,l_receiptdate,l_shipinstruct,l_shipmode,l_comment,temp" \
+            -H "Expect: 100-continue" \
             -T "$*" http://"${FE_HOST}":"${FE_HTTP_PORT}"/api/"${DB}"/lineitem/_stream_load
     fi
 }
