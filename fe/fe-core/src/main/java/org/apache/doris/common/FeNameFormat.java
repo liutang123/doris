bce36bee3f3f3f6077eef93b2c50c0f35f000204
derenli <derenli@tencent.com>
2025-04-21 20:55:53 +0800

    [Tencent] fix a build error in FeNameFormat.java

--- fe/fe-core/src/main/java/org/apache/doris/common/FeNameFormat.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/FeNameFormat.java
@@ -37,7 +37,7 @@ public class FeNameFormat {
     private static final String UNDERSCORE_COMMON_NAME_REGEX = "^[_a-zA-Z][a-zA-Z0-9-_]{0,63}$";
     private static final String TABLE_NAME_REGEX = "^[a-zA-Z][a-zA-Z0-9-_]*$";
     private static final String USER_NAME_REGEX = "^[a-zA-Z][a-zA-Z0-9.-_]*$";
-
+    private static final String REPOSITORY_NAME_REGEX = "^[a-zA-Z][a-zA-Z0-9-_]{0,255}$";
     private static final String CAM_USER_NAME_REGEX = "^[0-9a-zA-Z\\+\\=,\\.\\@\\-\\_]{1,64}$";
     private static final String COLUMN_NAME_REGEX = "^[_a-zA-Z@0-9\\s/][.a-zA-Z0-9_+-/?@#$%^&*\"\\s,:]{0,255}$";
 
