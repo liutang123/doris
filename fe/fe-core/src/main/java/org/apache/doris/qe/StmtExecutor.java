748c988829af382df86e096066e33ff0858898a5
camby <104178625@qq.com>
2025-08-21 08:39:49 +0800

    [Tencent] [fix](config) fix admin set all frontends config #55016 (#55072)
    pick #55016

--- fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -3192,7 +3192,7 @@ public class StmtExecutor {
 
     private void handleDdlStmt() {
         try {
-            DdlExecutor.execute(context.getEnv(), (DdlStmt) parsedStmt);
+            DdlExecutor.execute(context.getEnv(), (DdlStmt) parsedStmt, isProxy);
             if (!(parsedStmt instanceof AnalyzeStmt)) {
                 context.getState().setOk();
             }
