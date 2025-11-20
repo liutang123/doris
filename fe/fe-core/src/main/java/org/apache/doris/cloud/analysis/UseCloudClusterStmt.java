c5190e07e2b525966e0a05d8f3e65e112499666f
zhangdong <zhangdong@selectdb.com>
2025-11-20 18:22:20 +0800

    [Tencent][fix](auth)Fixed an issue where the wrong catalog was used when checking permissions with use cluster  (#58084) when switch catalog1; use `db1`@`group1`; check priv of internal.db1 should check priv of catalog1.db1

--- fe/fe-core/src/main/java/org/apache/doris/cloud/analysis/UseCloudClusterStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/cloud/analysis/UseCloudClusterStmt.java
@@ -28,12 +28,10 @@ import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.UserException;
-import org.apache.doris.datasource.InternalCatalog;
 import org.apache.doris.mysql.privilege.PrivPredicate;
 import org.apache.doris.qe.ConnectContext;
 
 import com.google.common.base.Strings;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -104,13 +102,15 @@ public class UseCloudClusterStmt extends StatementBase implements NotFallbackInP
             return;
         }
         if (!Env.getCurrentEnv().getAccessManager()
-                .checkDbPriv(ConnectContext.get(),
-                        StringUtils.isEmpty(catalogName) ? InternalCatalog.INTERNAL_CATALOG_NAME : catalogName,
-                        database,
+                .checkDbPriv(ConnectContext.get(), getAuthCatalogName(), database,
                         PrivPredicate.SHOW)) {
             ErrorReport.reportAnalysisException(ErrorCode.ERR_DBACCESS_DENIED_ERROR,
                     analyzer.getQualifiedUser(), database);
         }
+    }
+
+    public String getAuthCatalogName() {
+        return catalogName == null ? ConnectContext.get().getDefaultCatalog() : catalogName;
     }
 
     @Override
