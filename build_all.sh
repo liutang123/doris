c644be3e1fdfd2a85c4022d4699255bf68cbdd1e
derenli <derenli@tencent.com>
2026-01-23 15:51:15 +0800

    [Tencent](build) New add je-18.3.14-doris-SNAPSHOT.jar

--- build_all.sh
+++ build_all.sh
@@ -8,6 +8,14 @@ curdir=$(
 )
 WORK_DIR=$curdir
 
+JAR_DIR="${WORK_DIR}/cdw-doris-release/need_to_install_jar"
+mvn install:install-file  \
+  -DgroupId=org.apache.doris  \
+  -DartifactId=je \
+  -Dversion=18.3.14-doris-SNAPSHOT  \
+  -Dpackaging=jar  \
+  -Dfile=${JAR_DIR}/je-18.3.14-doris-SNAPSHOT.jar
+
 # clean and build all
 export JAVA_HOME=/usr/lib/jvm/jdk-17.0.2/
 export PATH=$JAVA_HOME/bin/:$PATH
